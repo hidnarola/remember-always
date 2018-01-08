f8cc1fdd472252d99d7b9c30918ac67d055be398
AKK <akk@narola.email>
2018-01-08 14:47:41 +0530

    Question related task,

---
+++ application/controllers/Questions.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Questions Controller
+ * Display search results
+ * @author KU 
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Questions extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct();
+//        $this->load->model('search_model');
+    }
+
+    /**
+     * Display questions pages.
+     */
+    public function index() {
+        
+    }
+
+}
