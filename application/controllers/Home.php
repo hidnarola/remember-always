5c681fef63718cac9f225ec7b83b46c746c035df
NAROLA\kunarola <ku@narola.email>
2018-02-12 15:26:46 +0530

    Email testing feature

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -23,71 +23,12 @@ class Home extends MY_Controller {
     }
 
     public function test() {
-        require_once(APPPATH . 'vendor/stripe/init.php');
-
-        $stripe = \Stripe\Stripe::setApiKey("sk_test_N9gt1bWxLOcFaoNGuYI1mCvk");
-    }
-
-    public function wepay() {
-        require_once(APPPATH . 'libraries/Wepay.php');
-        $this->load->view('wepay');
-    }
-
-    public function payment() {
-        require_once(APPPATH . 'libraries/Wepay.php');
-
-        // oauth2 parameters
-        $code = $this->input->post('code'); // the code parameter from step 2
-        $redirect_uri = site_url() . 'home/wepay/'; // this is the redirect_uri you used in step 1
-        // application settings
-        $client_id = "122816";
-        $client_secret = "1c97f56650";
-        // change to useProduction for live environments
-        Wepay::useProduction($client_id, $client_secret);
-        $wepay = new WePay(NULL); // we don't have an access_token yet so we can pass NULL here
-        // create an account for a user
-        $response = WePay::getToken($code, $redirect_uri);
-        $token = $response->access_token;
-        echo 'token is' . $token;
-        $wepay = new WePay($token);
-        $response = $wepay->request('account/create/', array(
-            'name' => 'Pav Account',
-            'description' => 'For testing account'
-        ));
-
-        // display the response
-        var_dump($response);
-        print_r($response);
-    }
-
-    public function checkout() {
-        // WePay PHP SDK - http://git.io/mY7iQQ
-        require_once(APPPATH . 'libraries/Wepay.php');
-        // application settings
-        $account_id = 1073237299; // your app's account_id
-        $client_id = 122816;
-        $client_secret = "727523550";
-        $access_token = "PRODUCTION_3c0034508d21778cd9123118f73964b514c6ab6d2b29f3b91aa82c448b47410b"; // your app's access_token
-        // change to useProduction for live environments
-        Wepay::useProduction($client_id, $client_secret);
-        $wepay = new WePay($access_token);
-        // create the checkout
-        $response = $wepay->request('checkout/create', array(
-            'account_id' => $account_id,
-            'amount' => '24.95',
-            'short_description' => 'Services rendered by freelancer',
-            'type' => 'service',
-            'currency' => 'USD'
-        ));
-        // display the response
-        $checkout_id = $response->checkout_id;
-        $checkout_uri = $response->checkout_uri;
-
-        print_r($response);
-    }
-
-    public function custompage() {
-        $this->load->view('custompage');
+        $email_data = [];
+        $email_data['url'] = site_url() . 'reset_password?code=123';
+        $email_data['name'] = 'Ku narola';
+        $email_data['email'] = 'ku@narola.email';
+        $email_data['subject'] = 'Reset Password - Remember Always';
+        send_mail('ku@narola.email', 'forgot_password', $email_data);
     }
 
 }
--- application/views/custompage.php
+++
@@ -1,7 +0,0 @@
-<div id="wepay_checkout"></div>
-<script type="text/javascript" src="https://www.wepay.com/min/js/iframe.wepay.js">
-</script>
-
-<script type="text/javascript">
-    WePay.iframe_checkout("wepay_checkout", "https://www.wepay.com/api/checkout/363214553/0b77bc00 ");
-</script>
