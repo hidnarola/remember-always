fbbefc5bfb03342254ae5866e963a67ae6140968
NAROLA\kunarola <ku@narola.email>
2018-02-12 17:42:42 +0530

    Helper configuration

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -25,8 +25,8 @@ function send_mail($to = '', $template = '', $data = []) {
     $config['protocol'] = 'smtp';
     $config['smtp_host'] = 'ssl://smtp.gmail.com';
     $config['smtp_port'] = '465';
-    $config['smtp_user'] = 'demo.narola@gmail.com';
-    $config['smtp_pass'] = 'Narola@21';
+    $config['smtp_user'] = 'pav.narola@gmail.com';
+    $config['smtp_pass'] = 'narola21';
     $config['charset'] = 'utf-8';
     $config['newline'] = "\r\n";
     $config['mailtype'] = 'html';
