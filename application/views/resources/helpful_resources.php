ff20d8705aa914c29f4372abac3097ef589e6adc
NAROLA\kunarola <ku@narola.email>
2018-02-22 14:58:06 +0530

    no message

--- application/views/resources/helpful_resources.php
+++ application/views/resources/helpful_resources.php
@@ -131,6 +131,9 @@
                         </ul>
                     </div>
                 </div>
+                <!--                <div class="profile-box fun-facts img_ad_custom">
+                                    <div class="adv_div"></div>
+                                </div>-->
                 <script type="text/javascript">
                     clicksor_mobile_redirect = false;
                     //default banner house ad url 
@@ -151,30 +154,6 @@
                     clicksor_layer_banner = false;
                 </script>
                 <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=389555&amp;adtype=5&amp;sid=652028"></script>
-<!--                <div class="profile-box fun-facts img_ad_custom">
-                    <div class="adv_div">
-                        <script type="text/javascript">
-                    clicksor_mobile_redirect = false;
-                    //default banner house ad url 
-                    clicksor_default_url = '';
-                    clicksor_banner_border = '#805066';
-                    clicksor_banner_ad_bg = '#FFFFFF';
-                    clicksor_banner_link_color = '#000000';
-                    clicksor_banner_text_color = '#666666';
-                    clicksor_banner_image_banner = true;
-                    clicksor_banner_text_banner = true;
-                    clicksor_layer_border_color = '#805066';
-                    clicksor_layer_ad_bg = '#FFFFFF';
-                    clicksor_layer_ad_link_color = '#000000';
-                    clicksor_layer_ad_text_color = '#666666';
-                    clicksor_text_link_bg = '';
-                    clicksor_text_link_color = '';
-                    clicksor_enable_text_link = false;
-                    clicksor_layer_banner = false;
-                        </script>
-                        <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=389555&amp;adtype=5&amp;sid=652028"></script>
-                    </div>
-                </div>-->
             </div>
         </div>
     </div>
