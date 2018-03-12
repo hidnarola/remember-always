d0f1acbd1f0650bd0ca93893ff6b011b64d918c6
NAROLA\kunarola <ku@narola.email>
2018-03-12 09:59:25 +0530

    Removed Clicksor advertise section

--- application/views/profile/profile_detail.php
+++ application/views/profile/profile_detail.php
@@ -732,26 +732,6 @@
                             <?php } ?>
                         </div>
                     </div>
-                    <script type="text/javascript">
-                        clicksor_mobile_redirect = false;
-                        //default banner house ad url 
-                        clicksor_default_url = '';
-                        clicksor_banner_border = '#805066';
-                        clicksor_banner_ad_bg = '#FFFFFF';
-                        clicksor_banner_link_color = '#000000';
-                        clicksor_banner_text_color = '#666666';
-                        clicksor_banner_image_banner = true;
-                        clicksor_banner_text_banner = true;
-                        clicksor_layer_border_color = '#805066';
-                        clicksor_layer_ad_bg = '#FFFFFF';
-                        clicksor_layer_ad_link_color = '#000000';
-                        clicksor_layer_ad_text_color = '#666666';
-                        clicksor_text_link_bg = '';
-                        clicksor_text_link_color = '';
-                        clicksor_enable_text_link = false;
-                        clicksor_layer_banner = false;
-                    </script>
-                    <script type="text/javascript" src="https://b.clicksor.net/show.php?nid=1&amp;pid=389555&amp;adtype=8&amp;sid=652028"></script>
                     <!--<div class="profile-box ad"></div>-->
                 </div>
             </div>
