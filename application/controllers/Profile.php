07fd2e89fa3ad46122bfd3cc488667a2221d0fc6
NAROLA\kunarola <ku@narola.email>
2018-03-23 11:08:01 +0530

    no message

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -142,7 +142,7 @@ class Profile extends MY_Controller {
                 $og_image = base_url() . PROFILE_IMAGES . $profile['profile_image'];
                 $image_size = getimagesize($og_image);
                 if ($image_size[0] < 200 || $image_size[1] < 200) {
-                    $og_image = base_url() . 'assets/timthumb.php?src=' . $og_image . '&zc=3&w=200&h=200&q=100';
+                    $og_image = base_url() . 'assets/timthumb.php?src=' . $og_image . '&zc=3&w=300&h=300&q=100';
                 }
                 $data['og_image'] = $og_image;
                 $data['breadcrumb'] = ['title' => 'Life Profile', 'links' => [['link' => site_url(), 'title' => 'Home'], ['link' => site_url('search'), 'title' => 'Profiles']]];
